f6fbc617afa44080e3e33d9d3d9ccf742379070b
Irina <irinaioanastoica@gmail.com>
2019-09-06 07:09:49 -0400

    Revert "Php format"
    This reverts commit 7dd3ad1872f958bf504f6dbced5bc07169432972.

--- scripts.php
+++
@@ -1,5 +0,0 @@
-<script type='text/javascript' src='assets/js/jquery-1.11.3.min.js'></script>
-<script type='text/javascript' src='assets/bootstrap/bootstrap.min.js'></script>
-<script src="assets/js/index.js"></script>
-<script src="assets/js/irina-animation-hover-script.js"></script>
-</html>
